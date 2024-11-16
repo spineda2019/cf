ce2f7f30cb2e5331ab551ceea4c35b9290e67979
spineda2019 <spineda.wpi.alum@gmail.com>
2024-11-16 15:35:04 -0500

    fix copyright

--- src/cf.asm
+++ src/cf.asm
@@ -1,5 +1,5 @@
 ;    cf: count the files in your current directory tree
-;    Copyright (C) <year>  <name of author>
+;    Copyright (C) 2024  Sebastian Pineda
 ;    
 ;    This program is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
